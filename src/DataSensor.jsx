b8aaeca118095d7bf37c8f1a1b6075807b13ce91
nachitoxx6262 <ignachio2003@hotmail.com>
2023-08-28 11:17:36 -0300

    add styles

---
+++ src/DataSensor.jsx
@@ -0,0 +1,114 @@
+import  { useEffect, useState } from "react";
+import { Typography, Container, Paper, Grid } from "@mui/material";
+import { DateTime } from "luxon";
+import axios from "axios";
+
+const DataSensor =()=>{
+    const [temperatura, setTemperatura] = useState("");
+    const [humedad, setHumedad] = useState("");
+    const [createat, setCreateat] = useState(DateTime.local());
+    const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzYnhvdHJlZXJyamFwZndiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI5MDIxNTUsImV4cCI6MjAwODQ3ODE1NX0.SUzX9mO4vZZ2xNmTD_6Y9MLzpFOHgfqxWKnWvMPdNsI";
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(
+            "https://usbxotreerrjapfwbaap.supabase.co/rest/v1/sensor_data?select=*",
+            {
+              headers: { "apikey": `${apikey}` },
+            }
+          );
+  
+          const data = response.data;
+          const ultimo = data.length - 1;
+          const last_data = data[ultimo];
+          setTemperatura(last_data.temperature);
+          setHumedad(last_data.humidity);
+          let time = last_data.created_at;
+          let realtime = DateTime.fromISO(time).setZone(
+            "America/Argentina/Buenos_Aires"
+          );
+          setCreateat(realtime);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+    return(
+        <Container sx={{ marginTop: "20px" }} maxWidth="md">
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: 120,
+                width: "100%",
+              }}
+            >
+              <Typography
+                style={{ fontFamily: "Signika Negative", fontWeight: "bold" }}
+                variant="h4"
+              >
+                Temperatura
+              </Typography>
+              <Typography
+                style={{ fontFamily: "Signika Negative" }}
+                component="p"
+                variant="h4"
+              >
+                🔥{temperatura} C°
+              </Typography>
+              <Typography
+                variant="body1"
+                style={{ fontFamily: "Signika Negative" }}
+              >
+                Ultima Actualización{" "}
+                {createat.toFormat("yyyy-MM-dd HH:mm:ss")}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: 120,
+                width: "100%",
+              }}
+            >
+              <Typography
+                style={{ fontFamily: "Signika Negative", fontWeight: "bold" }}
+                variant="h4"
+              >
+                Humedad
+              </Typography>
+              <Typography
+                style={{ fontFamily: "Signika Negative" }}
+                component="p"
+                variant="h4"
+              >
+                💧{humedad}%
+              </Typography>
+              <Typography
+                variant="body1"
+                style={{ fontFamily: "Signika Negative" }}
+              >
+                Ultima Actualización{" "}
+                {createat.toFormat("yyyy-MM-dd HH:mm:ss")}
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+
+    )
+}
+
+export default DataSensor;
--- src/Hidroponic.jsx
+++ src/Hidroponic.jsx
@@ -1,41 +1,9 @@
-import  { useEffect, useState } from "react";
-import { Box, Typography, Container, Paper, Grid } from "@mui/material";
-import { DateTime } from "luxon";
-import axios from "axios";
+
+import { Box, Typography, Container, Grid} from "@mui/material";
+import DataSensor from "./DataSensor";
+
 
 const Hidroponic = () => {
-  const [temperatura, setTemperatura] = useState("");
-  const [humedad, setHumedad] = useState("");
-  const [createat, setCreateat] = useState(DateTime.local());
-  const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzYnhvdHJlZXJyamFwZndiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI5MDIxNTUsImV4cCI6MjAwODQ3ODE1NX0.SUzX9mO4vZZ2xNmTD_6Y9MLzpFOHgfqxWKnWvMPdNsI";
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://usbxotreerrjapfwbaap.supabase.co/rest/v1/sensor_data?select=*",
-          {
-            headers: { "apikey": `${apikey}` },
-          }
-        );
-
-        const data = response.data;
-        const ultimo = data.length - 1;
-        const last_data = data[ultimo];
-        setTemperatura(last_data.temperature);
-        setHumedad(last_data.humidity);
-        let time = last_data.created_at;
-        let realtime = DateTime.fromISO(time).setZone(
-          "America/Argentina/Buenos_Aires"
-        );
-        setCreateat(realtime);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <Box
@@ -104,94 +72,54 @@ const Hidroponic = () => {
           }
         </Typography>
       </Box>
-
-      <Container
-        component="main"
-        sx={{ marginTop: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}
-        maxWidth="sm"
-      >
-        <Typography
-          color="white"
-          variant="h4"
-          component="h1"
-          textAlign="center"
-          style={{ fontFamily: "Signika Negative" }}
-          gutterBottom
-        >
-          Temperatura y Humedad dentro del invernadero
-        </Typography>
-      </Container>
-
-      <Container sx={{ marginTop: "20px" }} maxWidth="md">
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={12} sm={6} md={4}>
-            <Paper
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: 120,
-                width: "100%",
-              }}
-            >
-              <Typography
-                style={{ fontFamily: "Signika Negative", fontWeight: "bold" }}
-                variant="h4"
-              >
-                Temperatura
+      <Container >
+        <Grid container spacing={3} justifyContent="space-around" alignItems="center" marginTop={"-50px"} >
+          {/* Item 1 */}
+          <Grid item xs={12} sm={6} md={3}>
+            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center"  position="relative" sx={{zIndex: 2}}>
+              <Box bgcolor="white" borderRadius="50px" width="90px" height="90px" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                <img src="./plant.svg" width="45px" alt="Icono planta" />
+              </Box>
+              <Typography variant="h6" fontFamily="Signika Negative" fontWeight="400">
+                No necesita tierra
               </Typography>
-              <Typography
-                style={{ fontFamily: "Signika Negative" }}
-                component="p"
-                variant="h4"
-              >
-                🔥{temperatura} C°
-              </Typography>
-              <Typography
-                variant="body1"
-                style={{ fontFamily: "Signika Negative" }}
-              >
-                Ultima Actualización{" "}
-                {createat.toFormat("yyyy-MM-dd HH:mm:ss")}
-              </Typography>
-            </Paper>
+            </Box>
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Paper
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: 120,
-                width: "100%",
-              }}
-            >
-              <Typography
-                style={{ fontFamily: "Signika Negative", fontWeight: "bold" }}
-                variant="h4"
-              >
-                Humedad
+          {/* Repite lo anterior para los otros ítems */}
+          <Grid item xs={12} sm={6} md={3}>
+            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" position="relative" sx={{zIndex: 2}}>
+              <Box bgcolor="white" borderRadius="50px" width="90px" height="90px" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                <img src="./water.svg" width="35px" alt="Icono planta" />
+              </Box>
+              <Typography variant="h6" fontFamily="Signika Negative" fontWeight="400">
+              Ahorro de agua
               </Typography>
-              <Typography
-                style={{ fontFamily: "Signika Negative" }}
-                component="p"
-                variant="h4"
-              >
-                💧{humedad}%
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" position="relative" sx={{zIndex: 2}}>
+              <Box bgcolor="white" borderRadius="50px" width="90px" height="90px" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                <img src="./sand.svg" width="30px" alt="Icono planta" />
+              </Box>
+              <Typography variant="h6" fontFamily="Signika Negative" fontWeight="400">
+              x3-x4 más rapido
               </Typography>
-              <Typography
-                variant="body1"
-                style={{ fontFamily: "Signika Negative" }}
-              >
-                Ultima Actualización{" "}
-                {createat.toFormat("yyyy-MM-dd HH:mm:ss")}
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" position="relative" sx={{zIndex: 2}}>
+              <Box bgcolor="white" borderRadius="50px" width="90px" height="90px" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                <img src="./chart.svg" width="40px" alt="Icono planta" />
+              </Box>
+              <Typography variant="h6" fontFamily="Signika Negative" fontWeight="400">
+              80% rendimiento
               </Typography>
-            </Paper>
+            </Box>
           </Grid>
         </Grid>
       </Container>
+      
+      <DataSensor/>
     </Box>
   );
 };
